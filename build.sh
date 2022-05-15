5b56afe67d16a3a7cec53824531c55bf3ff315ad
tang jia <tangjia.jackis@qq.com>
2022-05-15 14:08:19 +0800

    添加环境搭建脚本

---
+++ build.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# 安装 bluez 依赖环境，可能有缺失，需要及时更新
+echo "Installing bluez dependency"
+sudo apt-get install -y git bc libusb-dev libdbus-1-dev libglib2.0-dev libudev-dev libical-dev libreadline-dev autoconf bison flex libssl-dev libjson-c-dev
+echo "Installing bluez dependency success"
+
+# 安装 bluez 依赖项目 ell，需与 bluez 处于同一目录下
+cd ~
+if ! [ -d ell-0.6 ] || [ -d ell ];
+then
+  echo "Installing ell"
+  wget https://mirrors.edge.kernel.org/pub/linux/libs/ell/ell-0.6.tar.xz
+  tar -xvf ell-0.6.tar.xz
+  cd ell-0.6/
+  ./configure --prefix=/usr
+  make
+  sudo make install
+  echo "Installing success"
+fi
+
+# 安装 Bluez
+cd ~
+if ! [ -d bluez-5.50 ] || [ -d bluez ];
+then
+  echo "Installing bluez"
+  wget http://www.kernel.org/pub/linux/bluetooth/bluez-5.50.tar.xz 
+  tar -xvf bluez-5.50.tar.xz 
+  cd bluez-5.50/
+  ./bootstrap-configure --disable-android --disable-midi
+  make
+  sudo systemctl daemon-reload
+  sudo systemctl restart bluetooth
+  echo "Installing success"
+fi
+
+# 安装 rtthread linux 环境
+sudo apt install libncurses5-dev python3
+pip3 install scons
+
+cd /tmp/
+if ! [ -d gcc-arm-none-eabi-6_2-2016q4 ];
+then
+  wget https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu-rm/6-2016q4/gcc-arm-none-eabi-6_2-2016q4-20161216-linux.tar.bz2
+  tar xf ./gcc-arm-none-eabi-6_2-2016q4-20161216-linux.tar.bz2
+  mv gcc-arm-none-eabi-6_2-2016q4/ /opt/
+  /opt/gcc-arm-none-eabi-6_2-2016q4/bin/arm-none-eabi-gcc --version
+fi
+
+# 安装 qemu
+# ubuntu 20.04 之后将 qemu-system-arm 剥离出了 qemu，需要单独安装
+sudo apt install qemu-system-arm
+if ! [ $? -eq 0 ];
+then
+  sudo apt install qemu
+fi
---
+++ qemu-ble.sh
@@ -0,0 +1,18 @@
+if [ ! -f "sd.bin" ]; then
+dd if=/dev/zero of=sd.bin bs=1024 count=65536
+fi
+
+# 关闭 hci0 设备，需替换为实际的 hci 设备，用 hciconfig 命令查看
+# $ hciconfig
+# hci0:	Type: Primary  Bus: USB
+# 	BD Address: 00:00:00:00:00:00  ACL MTU: 27:7  SCO MTU: 0:0
+# 	UP RUNNING 
+# 	RX bytes:514 acl:0 sco:0 events:45 errors:0
+# 	TX bytes:233 acl:0 sco:0 commands:45 errors:0
+sudo hciconfig hci0 down
+
+# 为 hci0 设备创建 unix socket 代理，并在子进程中运行，-i 0 代表 hci0 设备
+# bluez 需安装在用户家用目录 ~ 下，否则需手动修改
+sudo ~/bluez/tools/btproxy -u -i 0 &
+
+qemu-system-arm -M vexpress-a9 -smp cpus=2 -kernel rtthread.bin -nographic -sd sd.bin -serial mon:stdio -serial unix:/tmp/bt-server-bredr
